770aa614d3a1286e4c5b5bf84108a1070fb21b0e
unknown <Neilisop>
2022-03-11 13:01:31 +0530

    Getting ready to be public
    Signed-off-by: unknown <Neilisop>

--- bot.py
+++ bot.py
@@ -33,6 +33,8 @@ import humanize
 from datetime import datetime, timedelta
 from discord.commands import \
     slash_command
+import topgg
+from dbl import dbl_token
 
 load_dotenv()
 token = os.getenv("TOKEN")
@@ -40,13 +42,14 @@ prefix = os.getenv("PREFIX")
 
 os.chdir("./")
 
-async def get_prefix(bot, message):
+async def get_prefix(client, message):
     with open("prefix.json", "r") as f:
         prefixes = json.load(f)
 
     return prefixes[str(message.guild.id)]
 
-bot = commands.Bot(command_prefix=get_prefix)
+intents = discord.Intents.all()
+bot = commands.AutoShardedBot(shard_count = 5, command_prefix=get_prefix, intents = intents)
 bot.remove_command('help')
 
 for filename in os.listdir('./cogs'):
@@ -55,12 +58,28 @@ for filename in os.listdir('./cogs'):
 
 owner_ids = [702385226407608341, 929270204222046249]
 bot.blacklisted_users = []
+bot.topggpy = topgg.DBLClient(bot, dbl_token, autopost=True, post_shard_count=True)
 
-# On connect event is commented out because it messes with the registerations of slash commands
 # @bot.event
 # async def on_connect():
 #     print('Connected to Discord')
 #     await bot.change_presence(activity=discord.Activity(name=f"Bot is starting", url="https://www.twitch.tv/neilisop", type=discord.ActivityType.streaming))
+
+@bot.event
+async def on_autopost_success():
+    print(f"{Fore.BLUE} Posted server count {Fore.GREEN}{bot.topggpy.guild_count}{Fore.BLUE}, shard count {Fore.GREEN}{bot.shard_count}{Fore.BLUE}")
+
+@tasks.loop(seconds=120)
+async def syncguilds():
+    with open("prefix.json", "r") as f:
+        prefixes = json.load(f)
+    for i in bot.guilds:
+        if str(i.id) not in prefixes:
+            prefixes[str(i.id)] = ">"
+            current_time = datetime.now(pytz.timezone('Asia/Kolkata')) 
+            print(Fore.YELLOW + f"[{current_time.month}/{current_time.day}/{current_time.year} {current_time.hour}:{current_time.minute}:{current_time.second}] " + Fore.BLUE + "[Non-prefixed guild checker] Detected a guild that is non-prefixed. Adding " + Fore.GREEN + f"{i}" + Fore.BLUE + " to prefix list.")
+    with open("prefix.json", "w") as f:
+        json.dump(prefixes, f, indent=4)
 
 @bot.slash_command(name="botinfo", description="Shows info about Edith")
 async def botinfo(ctx):
@@ -73,9 +92,11 @@ async def botinfo(ctx):
     now = datetime.now()
     uptime = launched_at - now
     humanized_time = humanize.precisedelta(uptime)
+    shard_id = ctx.guild.shard_id
+    shard_servers = len([guild for guild in bot.guilds if guild.shard_id == shard_id])
 
     embed = discord.Embed(title="Statistics:", 
-    description=f"**Username:** Edith\n**Discriminator:** 1574\n**Servers:** {guild_count}\n**Users:** {total_member_count}\n**Bot Version:** 2.0 \n**Uptime:** {humanized_time}\n**Client Status:**`🟢 Online` - `{ping}`\n**Library:** Discord.py - `{discord.__version__}`\n\n\n:warning: **We've shifted to Slash Commands! All Normal commands will not work from 1 March 12:00PM IST. If the Slash Commands don't appear, please reinvite the bot with this link: https://discord.com/oauth2/authorize?client_id=731807331796385812&permissions=1642824466295&scope=bot%20applications.commands. **",
+    description=f"**Username:** Edith\n**Discriminator:** 1574\n**Servers:** {guild_count}\n**Users:** {total_member_count}\n**Bot Version:** 2.0 \n**Uptime:** {humanized_time}\n**Client Status:**`🟢 Online` - `{ping}`\n**Library:** Discord.py - `{discord.__version__}`\n\n**Shard info**\nShard ID: {shard_id}\nShard servers: {shard_servers}\n\n\n:warning: **We've shifted to Slash Commands! All Normal commands will not work from 1 March 12:00PM IST. If the Slash Commands don't appear, please reinvite the bot with this link: https://discord.com/oauth2/authorize?client_id=731807331796385812&permissions=1642824466295&scope=bot%20applications.commands. **",
     color=discord.Color.blue())
     embed.set_thumbnail(url=bot.user.avatar)
     await ctx.respond(embed=embed)
@@ -148,7 +169,7 @@ def insert_returns(body):
 async def blacklist(ctx, user: discord.Member):
     if ctx.author.id in owner_ids:
         bot.blacklisted_users.append(user.id)
-        data = read_json("blacklist")
+        data = read_json(blacklist)
         data["blacklistedUsers"].append(user.id)
         write_json(data, "blacklist")
         e = discord.Embed(title="Success! :white_check_mark:", description=f"{ctx.author.name} has been blacklisted!", color=discord.Color.green())
@@ -194,7 +215,7 @@ async def prefix(ctx, prefix):
     prefixes[str(ctx.guild.id)] = prefix
 
     with open("prefix.json", "w") as f:
-        json.dump(prefixes, f, indent=4)    
+        json.dump(prefixes,f)    
 
     e = discord.Embed(title=":white_check_mark: Success!", description=f"The prefix was changed to {prefix}", color=discord.Color.green())
     await ctx.send(embed=e)
@@ -230,23 +251,17 @@ async def on_guild_remove(guild):
 
 """Blacklist Stuff"""
 
-ownerid1 = owner_ids[0]
-ownerid2 = owner_ids[1]
-
 @bot.event
-async def on_ready():
+async def on_shard_ready(shard_id):
     current_time = datetime.now(pytz.timezone('Asia/Kolkata'))
-    print(Fore.YELLOW + f"[{current_time.month}/{current_time.day}/{current_time.year} {current_time.hour}:{current_time.minute}:{current_time.second}] " + Fore.BLUE + f"[INFO] Logged in as " + Fore.YELLOW + f"{bot.user}")
+    print(Fore.YELLOW + f"[{current_time.month}/{current_time.day}/{current_time.year} {current_time.hour}:{current_time.minute}:{current_time.second}] " + Fore.BLUE + f"[INFO] Shard" + Fore.YELLOW + f" {shard_id} " + Fore.BLUE + "has logged in!")
     data = read_json("blacklist")
     bot.blacklisted_users = data["blacklistedUsers"]
-    channel = bot.get_channel(936669729777680404)
-    e = discord.Embed(title="Bot is online!", description=f"Owners: <@{ownerid1}> and <@{ownerid2}>\nBlacklisted users = {bot.blacklisted_users}", color=discord.Color.green(), timestamp=datetime.utcnow())
-    e.set_footer(text='\u200b',icon_url="https://cdn.discordapp.com/attachments/929652173682409482/936227655584452668/IMG_6767.png")
-    await channel.send(embed=e)
     global launched_at
     launched_at = datetime.now()
     print(Fore.RED)
-    syncguilds.start()
+# The sync guilds does not start at on_shard_ready or it will be ran everytime one shard is ready
+syncguilds.start()
 
 @bot.event
 async def on_message(message):
@@ -276,31 +291,61 @@ async def on_command_error(ctx, error):
     else:
         print(error)
 
-@bot.event
-async def on_application_command_error(ctx, error):
-    if isinstance(error, commands.CommandOnCooldown):
-        embed= discord.Embed(title="Slow Down :raised_back_of_hand:", color=discord.Color.red())
-        embed.add_field(name="This command is on cooldown!", value="Use this command again in  {:.2f}s".format(error.retry_after), inline=False)
-        await ctx.respond(embed=embed)
-    elif isinstance(error, commands.CheckFailure):
-        em = discord.Embed(title="No permission :x:", description="You don't have enough permission(s) to execute that command!", color=discord.Color.red())
-        await ctx.respond(embed=em)
-    else:
-        print(error)
-
-@tasks.loop(seconds=120)
-async def syncguilds():
-    with open("prefix.json", "r") as f:
-        prefixes = json.load(f)
-    for i in bot.guilds:
-        if str(i.id) not in prefixes:
-            prefixes[str(i.id)] = ">"
-            current_time = datetime.now(pytz.timezone('Asia/Kolkata')) 
-            print(Fore.YELLOW + f"[{current_time.month}/{current_time.day}/{current_time.year} {current_time.hour}:{current_time.minute}:{current_time.second}] " + Fore.BLUE + "[Non-prefixed guild checker] Detected a guild that is non-prefixed. Adding " + Fore.GREEN + f"{i}" + Fore.BLUE + " to prefix list.")
-    with open("prefix.json", "w") as f:
-        json.dump(prefixes, f, indent=4)
-
 """Helper Functions"""
+
+@bot.command()
+async def setstatus(ctx, type, *, status):
+    if ctx.author.id in owner_ids:
+        if type == "streaming":
+            e = discord.Embed(title="Stopping auto-status change events", color=discord.Color.red())
+            msg = await ctx.send(embed=e)
+            await asyncio.sleep(3)
+            bot.unload_extension("cogs.status")
+            statusstopped = discord.Embed(title="Auto-Status change has been stopped!", color=discord.Color.orange())
+            await msg.edit(embed=statusstopped)
+            await asyncio.sleep(2)
+            await bot.change_presence(activity=discord.Activity(name=f"{status}", url="https://www.twitch.tv/neilisop", type=discord.ActivityType.streaming))
+            statuschanged = discord.Embed(title="Bot status has been changed!", color=discord.Color.green())
+            await msg.edit(embed=statuschanged)
+        elif type == "playing":
+            e = discord.Embed(title="Stopping auto-status change events", color=discord.Color.red())
+            msg = await ctx.send(embed=e)
+            await asyncio.sleep(3)
+            bot.unload_extension("cogs.status")
+            statusstopped = discord.Embed(title="Auto-Status change has been stopped!", color=discord.Color.orange())
+            await msg.edit(embed=statusstopped)
+            await asyncio.sleep(2)
+            await bot.change_presence(activity=discord.Activity(name=f"{status}", type=discord.ActivityType.playing))
+            statuschanged = discord.Embed(title="Bot status has been changed!", color=discord.Color.green())
+            await msg.edit(embed=statuschanged)
+        elif type == "listening":
+            e = discord.Embed(title="Stopping auto-status change events", color=discord.Color.red())
+            msg = await ctx.send(embed=e)
+            await asyncio.sleep(3)
+            bot.unload_extension("cogs.status")
+            statusstopped = discord.Embed(title="Auto-Status change has been stopped!", color=discord.Color.orange())
+            await msg.edit(embed=statusstopped)
+            await asyncio.sleep(2)
+            await bot.change_presence(activity=discord.Activity(name=f"{status}", type=discord.ActivityType.listening))
+            statuschanged = discord.Embed(title="Bot status has been changed!", color=discord.Color.green())
+            await msg.edit(embed=statuschanged)
+        elif type == "watching":
+            e = discord.Embed(title="Stopping auto-status change events", color=discord.Color.red())
+            msg = await ctx.send(embed=e)
+            await asyncio.sleep(3)
+            bot.unload_extension("cogs.status")
+            statusstopped = discord.Embed(title="Auto-Status change has been stopped!", color=discord.Color.orange())
+            await msg.edit(embed=statusstopped)
+            await asyncio.sleep(2)
+            await bot.change_presence(activity=discord.Activity(name=f"{status}", type=discord.ActivityType.watching))
+            statuschanged = discord.Embed(title="Bot status has been changed!", color=discord.Color.green())
+            await msg.edit(embed=statuschanged)
+        else:
+            error = discord.Embed(title=":x: Error!", description="You must provide a status type and status value.\n**Syntax**\n`>setstatus listening dua lipa`", color=discord.Color.red())
+            await ctx.respond(embed=error)
+    else:
+        e = discord.Embed(title=":x: You are not allowed to run this command!", color=discord.Color.red())
+        await ctx.respond(embed=e)
 
 @bot.slash_command(name="setstatus", description="Set the bots status to something!")
 async def setstatus(ctx, type, *, status):
@@ -356,7 +401,7 @@ async def setstatus(ctx, type, *, status):
         e = discord.Embed(title=":x: You are not allowed to run this command!", color=discord.Color.red())
         await ctx.respond(embed=e)
 
-# Helper functions for interacting with json files.
+
 def read_json(filename):
     with open(f"{filename}.json", "r") as f:
         data = json.load(f)
@@ -615,4 +660,5 @@ async def joke(ctx):
     	title="Joke", description="Tells you a joke.", color=discord.Color.blue())
     embed.add_field(name="**Syntax**", value=">joke")
     await ctx.send(embed=embed)
+
 bot.run(token)
